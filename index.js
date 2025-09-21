27239ecd71a353fabc4ba13a62a4284b85a5ac71
tjokic989 <tomajokic1989@gmail.com>
2025-09-21 21:17:47 +0200

    I wrote js and html

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Click Counter
 
+const btn = document.getElementById('btn');
+const countEl = document.getElementById('count');
+let count = 0;
+
+btn.addEventListener('click', () => {
+    count += 1;
+    countEl.textContent = count;
+})
